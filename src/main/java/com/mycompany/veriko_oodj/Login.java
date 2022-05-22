06f97266ca17fcfe9fd27b8427a279b975d40204
RoNIN961 <75106650+RoNIN961@users.noreply.github.com>
2022-05-22 22:15:46 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/veriko_oodj/Login.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.veriko_oodj;
+
+/**
+ *
+ * @author rong0
+ */
+public class Login {
+    public static void main(String[] args){
+        LoginFrame lf = new LoginFrame();
+        lf.setVisible(true);
+    }
+}
